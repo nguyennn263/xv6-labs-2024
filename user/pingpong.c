052930b23d4630c618119cb4814161b838fccdfb
TamBeo2103 <lelynhan2@gmail.com>
2024-10-26 00:07:51 +0700

    Cập nhật file pingpong.c (dùng 1 pipe)

--- user/pingpong.c
+++ user/pingpong.c
@@ -2,12 +2,11 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int main(void) {
-    int p1[2]; // Tạo pipe từ parent -> child
-    int p2[2]; // Tạo pipe từ child -> parent
+int main() {
+    int p[2]; // Tạo pipe
     char buf[1];
     
-    if (pipe(p1) < 0 || pipe(p2) < 0) {
+    if (pipe(p)<0) {
         fprintf(2, "pipe failed\n");
         exit(1);
     }
@@ -19,45 +18,31 @@ int main(void) {
     }
 
     if (pid == 0) { // child process
-        close(p1[1]); // đóng write end của p1
-        close(p2[0]); // đóng read end của p2
+        close(p[1]); // đóng write end của pipe
         
         // Đọc byte
-        if (read(p1[0], buf, 1) != 1) {
+        if (read(p[0], buf, 1) != 1) {
             fprintf(2, "child read failed\n");
             exit(1);
         }
         printf("%d: received ping\n", getpid());
 
-        // Gửi byte
-        if (write(p2[1], buf, 1) != 1) {
-            fprintf(2, "child write failed\n");
-            exit(1);
-        }
-
-        close(p1[0]);
-        close(p2[1]);
+        close(p[0]);
         exit(0);
     } else { // parent process
-        close(p1[0]); 
-        close(p2[1]);
+        close(p[0]); 
 
         // Gửi byte
-        if (write(p1[1], "a", 1) != 1) {
+        if (write(p[1], "a", 1) != 1) {
             fprintf(2, "parent write failed\n");
             exit(1);
         }
 
-        // Đọc byte
-        if (read(p2[0], buf, 1) != 1) {
-            fprintf(2, "parent read failed\n");
-            exit(1);
-        }
+        wait(0); // đợi child process kết thúc
+
         printf("%d: received pong\n", getpid());
 
-        close(p1[1]);
-        close(p2[0]);
-        wait(0); // đợi child process kết thúc
+        close(p[1]);
         exit(0);
     }
 }
