ac67d62c13d9a1f8f2535afd50e4ef6e3d681b08
nguyennn263 <ngtrungquoc263@gmail.com>
2024-10-25 23:15:50 +0700

    update comment in task 2 and 4

--- user/primes.c
+++ user/primes.c
@@ -13,8 +13,10 @@ int process(int in_fd, int prime) {
 	int num;
 	int pid = fork();
 	if (pid == 0) {
+		// Lan luot sang so nguyen to
 		while (read(in_fd, &num, sizeof(int))) {
 			if (num % prime != 0) {
+				// Tranh truong hop pipe bi day.
 				while(write(out_pipe, &num, sizeof(int)) == -1){
 					
 				}
@@ -35,11 +37,15 @@ int process(int in_fd, int prime) {
 int main(int argc, char *argv[]) {
 	int p[2];
 	pipe(p);
+// Boi vi pipe chi chua duoc 512 byte tuong ung voi 128 so nguyen -> khong the viet het 279 so vao pipe
+// Do do can phai vua ghi vua doc tren pipe. 
+// Co the thay duoc write chi co 1 thao tac, nhung read thi se co nhieu thao tac hon, do do co the write nhanh hon read
+// -> pipe co the se bi day
+
     int pid = fork();
 	if (pid == 0) {
 		for (int i = FIRST_PRIME; i <= LAST_PRIME; i++) {
-			// printf("test %d\n", i);
-			
+			// Tranh truong hop pipe bi day.
 			while(write(p[1], &i, sizeof(int)) == -1){
 
 			}
@@ -52,6 +58,7 @@ int main(int argc, char *argv[]) {
 
 		int in_pipe = p[0];
 		int prime; 
+		// Doc pipe de in ra so nguyen to va goi fork.
 		while (read(in_pipe, &prime, sizeof(int))) {
 			printf("prime %d\n", prime); 
 			in_pipe = process(in_pipe, prime);
--- user/xargs.c
+++ user/xargs.c
@@ -1,14 +1,14 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-#include "kernel/param.h"  // MAXARG
+#include "kernel/param.h"
 
 int is_blank(char ch) {
     return (ch == ' ' || ch == '\t');
 } 
 
 int main(int argc, char *argv[]) {
-	char buffer[2048];
+	char buffer[2222];
     char *p_buffer = buffer;
     char ch;
 	char *arg[MAXARG];
@@ -16,9 +16,8 @@ int main(int argc, char *argv[]) {
 	int blank = 0;
 	int len = 0;
 
-
-	if(argc <= 1){
-		fprintf(2, "Thieu tham so");
+	if(argc < 2){
+		fprintf(2, "Thieu tham so: xargs <command> arg...");
 		exit(1);
 	}
 
@@ -26,12 +25,14 @@ int main(int argc, char *argv[]) {
 		arg[i-1] = argv[i];
 	}
 
+    // Doc lan luot ki tu
 	while (read(0, &ch, 1) > 0) {
         if(is_blank(ch)){
             blank = 1;
             continue;
-        } 
+        }
 
+        // Them 1 argument moi neu truoc do co blank
         if(blank){
 			buffer[len++] = '\0';
 
@@ -40,6 +41,7 @@ int main(int argc, char *argv[]) {
 			blank = 0;
         }
 
+        // Neu xuong dong thi phai fork va thuc thi lenh
 		if (ch != '\n') {
 			buffer[len++] = ch;
 		} 
@@ -49,12 +51,11 @@ int main(int argc, char *argv[]) {
 
             int pid = fork();
 			if (pid == 0) {
-				exit(exec(arg[0], arg));
+                exec(arg[0], arg);
+				exit(0);
 			}
 			wait(0);
-			
 			num_arg = argc - 1;
-
 		}
 	}
 
